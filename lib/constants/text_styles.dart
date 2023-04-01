577394adbff168319b4352855401ba1ff8cd12ec
RyoNakahara <ryo072144@gmail.com>
2023-04-02 03:22:06 +0900

    exercise

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class AppTextStyles{
+
+  static const TextStyle title = TextStyle(
+      color: AppColors.textColor,
+      fontSize: 20 ,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle body = TextStyle(
+    color: AppColors.textColor,
+    fontSize: 16 ,
+  );
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_exercise_3/constants/text_styles.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  //変数の頭の_は、この変数がこのクラスの内部でしか使えないということを意味する。
+  int _counter = 0;
+  bool _isToggleOn = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+
+          //条件式?値1:値2で、if(条件式){return 値1;}else{return 値2;}と同じ意味。
+          child: Icon(_isToggleOn?Icons.toggle_on_outlined:Icons.toggle_off_outlined, color: Colors.white, size: 40,),
+          onPressed: (){},
+        ),
+        title: const Text('タイトル', style: AppTextStyles.title,),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: (){
+                  },
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+                  ),
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20,),
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+                  ),
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
